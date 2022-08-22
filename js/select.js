db89361b53fc32482c145d98429fc5f421c609c1
Risan Ahamed <risanzahamed76@gmail.com>
2022-08-23 03:17:29 +0600

    click handler added

--- js/select.js
+++ js/select.js
@@ -34,4 +34,41 @@ function playerClick (element){
     display(playerArray)
 }
 
+document.getElementById("calculate-player").addEventListener("click" , function(){
+    const playerCalculate = document.getElementById("calculate-input");
+    const playerCalculateString = playerCalculate.value;
+    const NewPlayerAmount = parseFloat(playerCalculateString)
 
+    const playerExpensesElement = document.getElementById("player-expenses");
+    const playerExpensesString = playerExpensesElement.innerText;
+    const previousPlayerExpenses = parseFloat(playerExpensesString)
+
+    const currentBalanceTotal = playerArray.length * NewPlayerAmount;
+    playerExpensesElement.innerText = currentBalanceTotal
+
+    playerCalculate.value = '';
+
+    document.getElementById("calculate-total").addEventListener("click", function(){
+        const managerInput = document.getElementById("manager-input");
+        const managerInputCalculateString = managerInput.value;
+        const NewManagerCalculateAmount = parseFloat(managerInputCalculateString)
+    
+        const coachInput = document.getElementById("coach-input");
+        const coachInputCalculateString = coachInput.value;
+        const NewCoachCalculateAmount = parseFloat(coachInputCalculateString)
+        
+        const totalPriceElement = document.getElementById("total-price");
+        const totalPriceElementString = totalPriceElement.innerText;
+        const previoustotalPrice = parseFloat(totalPriceElementString)
+    
+        const NewBalanceTotal =(NewManagerCalculateAmount + previoustotalPrice + NewCoachCalculateAmount);
+        totalPriceElement.innerText = NewBalanceTotal
+    
+        const totalPriceAmount = NewBalanceTotal + currentBalanceTotal
+        totalPriceElement.innerText = totalPriceAmount
+    
+        managerInput.value = '';
+        coachInput.value = '';
+    })
+    
+})
